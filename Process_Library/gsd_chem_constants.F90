2eb04ff7dff552bf523809e2fff54b291a414314
Dom Heinzeller <climbfuji@ymail.com>
2021-04-23 09:53:41 -0600

    Add remaining GSD chem modules

--- Process_Library/gsd_chem_constants.F90
+++ Process_Library/gsd_chem_constants.F90
@@ -11,5 +11,6 @@ module gsd_chem_constants
   real(kind=kind_chem),parameter:: con_rv     =4.6150e+2_kind_chem                 !< gas constant H2O (\f$J/kg/K\f$)
   real(kind=kind_chem),parameter:: con_cp     =1.0046e+3_kind_chem                 !< spec heat air at p (\f$J/kg/K\f$)
   real(kind=kind_chem),parameter:: con_cv     =7.1760e+2_kind_chem                 !< spec heat air at v (\f$J/kg/K\f$)
+  real(kind=kind_chem),parameter:: con_pi     =4.0d0*atan(1.0d0)                   !< pi
 
 end module gsd_chem_constants
